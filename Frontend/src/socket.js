321d17edf516e2d00e2d3f152e9b3574743a1b25
Abhinav20singh03 <abhinav20singh03@gmail.com>
2025-03-07 15:59:33 +0530

    Final Commit

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,11 +1,11 @@
-import { io } from "socket.io-client";
+import {io} from "socket.io-client";
 
 export const initSocket = async () => {
-    const options = {
-        forceNew: true,  
-        reconnectionAttempts: 5,  
-        timeout: 10000,
-        transports: ["websocket"]
+    const option = {
+        "force new connection" : true,
+         reconnectionAttempt : true,
+         timeout : 10000,
+         transports : ["websocket"],
     };
-    return io(import.meta.env.VITE_BACKEND_URL || "https://devsync-xxt8.onrender.com", options);
-};
+    return io(import.meta.env.VITE_APP_BACKEND_URL,option);
+}
